3afd9a989e9daac42d82df8d7f41b13051364da7
Chris Kotze <kotwebck@gmail.com>
2015-03-02 18:57:42 +0200

    pre-dev setup
    Testing the debug code and welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">Jane of Tredos</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
